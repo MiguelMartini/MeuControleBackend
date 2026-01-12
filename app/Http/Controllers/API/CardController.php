73f584adab98aa1a577034d452785f0af96455ce
MiguelMartini <miguelkaulingm21@gmail.com>
2026-01-12 15:46:50 -0300

    Feat: Card Features: Store and Show

---
+++ app/Http/Controllers/API/CardController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Card;
+use App\Services\CardService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use League\Config\Exception\ValidationException;
+
+use function PHPUnit\Framework\isEmpty;
+
+class CardController extends Controller
+{
+    public function __construct(
+        private CardService $cardService
+    ) {}
+
+    public function index()
+    {
+        $user = Auth::user();
+        if (!$user) {
+            return response()->json([
+                'status' => 'Falha',
+                'message' => 'Usuário não encontrado'
+            ], 400);
+        }
+
+        $cards = $this->cardService->getCardsByUser($user);
+
+        if ($cards->isEmpty()) {
+            return response()->json([
+                'status' => 'Falha',
+                'message' => 'Nenhum Cartão cadastrado ainda.',
+                'data' => []
+            ], 200);
+        }
+
+        return response()->json([
+            'status' => 'Sucesso',
+            'data' => $cards
+        ], 200);
+    }
+    public function store(Request $request)
+    {
+        $user = Auth::user();
+
+        if (!$user) {
+            return response()->json([
+                'status' => 'Falha',
+                'message' => 'Usuário não encontrado'
+            ], 400);
+        }
+
+        try {
+            $card = $this->cardService->storeCard($user, $request);
+
+            return response()->json([
+                'status' => 'Sucesso',
+                'message' => 'Cartão cadastrado com sucesso',
+                'data' => $card->only(['title', 'description', 'card_type'])
+            ], 201);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'status' => 'Falha',
+                'message' => $e->errors()
+            ], 422);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $user = Auth::user();
+
+        if (!$user) {
+            return response()->json([
+                'status' => 'Falha',
+                'message' => 'Erro ao buscar usuário'
+            ], 404);
+        }
+        try {
+            $card = $this->cardService->getCardById($id, $user->id);
+
+            return response()->json([
+                'status' => 'Sucesso',
+                'message' => 'Cartão carregado com sucesso',
+                'data' => $card->only(['id', 'title', 'description', 'card_type'])
+            ], 200);
+        } catch (ModelNotFoundException  $e) {
+            return response()->json([
+                'status' => 'Falha',
+                'message' => 'Cartão não encontrado'
+            ], 404);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Services/CardService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Card;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Request;
+
+class CardService
+{
+    public function getCardsByUser (User $user){
+        return Card::where('user_id', $user->id)->select('id', 'title', 'description', 'card_type')->get();
+    }
+
+    public function storeCard(User $user, Request $request){
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:30',
+            'description' => 'nullable|string|max:50',
+            'card_type' => 'nullable|in:Debit,Credit'
+        ],[
+            '*.required' => 'O campo :attribute é obrigatório',
+            '*.max' => 'O campo :attribute deve possuir no máximo :max caractéres',
+            '*.in' => 'Apenas os seguintes valores são válidos: :values'
+        ], [
+            'title' => 'Título',
+            'description' => 'Descrição',
+            'card_type' => 'Tipo do cartão'
+        ]);
+
+        if($validator->fails()){
+            throw new ValidationException($validator);
+        }
+
+        return Card::create([
+            'user_id' => $user->id,
+            ...$validator->validated()
+        ]);
+    }
+
+    public function getCardById(int $card_id ,int $user_id){
+       return Card::where('id', $card_id)->where('user_id', $user_id)->firstOrFail();
+    }
+}
